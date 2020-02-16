745c0dcfcddc5cef2e20679c954591f74b49efd8
Gilbert Martinez <gilbert.martinez675@utexas.edu>
2020-02-16 15:06:18 -0600

    added error plot for flight d hw 3

--- 367K/hw3/4.py
+++ 367K/hw3/4.py
@@ -16,9 +16,9 @@ for alt in altitude:
         density_true[i] = (tau**-35.164)*2.2099e87
     i = i+1
 
-fig, ax = plt.subplots()
+fig, ax1 = plt.subplots()
 
-ax.plot(altitude, density_true, label="True")
+ax1.plot(altitude, density_true, label="True")
 
 rho_s = 2.3769e-3
 rho_t = 7.0613e-4
@@ -36,7 +36,20 @@ for alt in altitude:
         density_est[i] = rho_plus*np.e**(-(alt-h_plus)/20770)
     i = i+1
 
-ax.plot(altitude, density_est, label="Exponential")
-ax.legend()
+ax1.plot(altitude, density_est, label="Exponential")
+ax1.set_xlabel("Altitude [ft]")
+ax1.set_ylabel("Density[slugs/ft^3]")
+ax1.set_title("Atmospehric Density vs. Altitude")
+ax1.legend()
+fig2, ax2 = plt.subplots()
+percent_error = np.empty(np.shape(density_true))
+j = 0
+for density in density_true:
+    percent_error[j] = (np.abs(density - density_est[j])/density)*100
+    j=j+1
+ax2.plot(altitude, percent_error)
+ax2.set_xlabel("Altitude [ft]")
+ax2.set_ylabel("Percent Error")
+ax2.set_title("Accuracy of Exponential model vs True Model")
 plt.show()
 
