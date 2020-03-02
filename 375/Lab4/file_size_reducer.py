937a51e16c7b62bbe2bae2dd2c716a064beb6934
Gilbert Martinez <gilbert.martinez675@utexas.edu>
2020-03-02 17:10:22 -0600

    reduced file sizes significantly

---
+++ 375/Lab4/file_size_reducer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import time
+import numpy as np
+
+start = time.time()
+file = pd.read_excel('data/part2.xlsx')
+end = time.time()
+print("Data Read! It took:", end-start, "seconds")
+new_df = []
+# count = 0
+for index, row in file.iterrows():
+    if np.mod(index, 5) == 0:
+        new_df.append(row)
+print("reduction complete, writing to csv....")
+new_df = pd.DataFrame(new_df)
+new_df.to_csv('data/part2_reduced.csv')
+print("Operation Complete!")
