bf17767bff64815c7ab85cbafa91d41e10e8ed1c
Gilbert Martinez <gilbert.martinez675@utexas.edu>
2020-03-11 12:16:02 -0500

    there are two wings!

--- 367K/hw5/3.py
+++ 367K/hw5/3.py
@@ -12,19 +12,20 @@ C_L = (2*W)/(density*V**2*S)
 viscosity = (2.27e-8*temp**(3/2))/(temp+198.6)
 
 # [W, H, V, B, T, N] # Ignore wing nacelles
-IF = [1.2, 1.1, 1.1, 1.2,  1.25, 1.52]
-FF = [1+1.6*.09+100-.09**4, 1+1.6*.08+100*.08**4, 1+1.6*.1+100*.1**4, 1+60/(41/5.250)**3+0.0025*(41/5.25), 1+60/(14*1.75)**3+.0025*(14/1.75), 1+0.35/(7.7/2.3)]
+IF = [1.2, 1.1, 1.1, 1.2,  1.25, 1.5]
+FF = [1+1.6*.09+100*.09**4, 1+1.6*.08+100*.08**4, 1+1.6*.1+100*.1**4, 1+60/(41/5.250)**3+0.0025*(41/5.25), 1+60/(14*1.75)**3+.0025*(14/1.75), 1+0.35/(7.7/2.3)]
 S_wet = [344, 108, 75.4, 456, 61.2, 55.6]
 l = [7, 3.83, 6.92, 41, 14, 7.7]
 
-# there are two tip tanks and nacelles
-steps = [0, 1, 2, 3, 4, 4, 5, 5]
+# there are two wings,  tip tanks and nacelles
+steps = [0, 0, 1, 2, 3, 4, 4, 5, 5]
 f = 0
 for step in steps:
     Re = density*V*l[step]/viscosity
     C_f = 0.455/(np.log10(Re))**2.58
     CF = (1+0.2*M**2)**-0.467
-    f = f+C_f*CF*IF[step]*FF[step]*S_wet[step]
+    f_k = C_f*CF*IF[step]*FF[step]*S_wet[step]
+    f = f+f_k
 C_D_f = 1.1*f/S
 print("This is C_D_f: ", C_D_f)
 
--- 367K/hw5/4.py
+++ 367K/hw5/4.py
@@ -22,13 +22,22 @@ def density(alt):
     if alt > 65617:
         return rho_plus*np.e**(-(alt-h_plus)/20770)
 
+
+linVec = ['-', '--',':']
+
 d = np.empty([np.size(velocity), 3])
 
 d[:, 0] = np.multiply(.5*Cdo*density(altitude[0])*S, np.power(velocity, 2))+np.divide(2*K*L**2, np.multiply(S*density(altitude[0]), np.power(velocity, 2)))
 d[:, 1] = np.multiply(.5*Cdo*density(altitude[1])*S , np.power(velocity, 2))+np.divide(2*K*L**2, np.multiply(S*density(altitude[1]), np.power(velocity, 2)))
 d[:, 2] = np.multiply(.5*Cdo*density(altitude[2])*S , np.power(velocity, 2))+np.divide(2*K*L**2, np.multiply(S*density(altitude[2]), np.power(velocity, 2)))
-
-plt.plot(velocity, d)
+for i in [0,1,2]:
+    plt.plot(velocity, d[:, i], linestyle=linVec[i], color='k')
+plt.title("Total Drag on a SBJ")
+plt.ylabel("Drag [lbs]")
+plt.xlabel("Velocity [ft/s]")
+plt.legend(["10,000 ft", "25,000 ft", "35,000 ft"])
+plt.grid(True)
+plt.xlim(100, 900)
 
 plt.figure()
 
@@ -37,7 +46,15 @@ d_p[:, 0] = np.multiply(.5*Cdo*density(altitude[0])*S, np.power(velocity, 2))
 d_p[:, 1] = np.multiply(.5*Cdo*density(altitude[1])*S, np.power(velocity, 2))
 d_p[:, 2] = np.multiply(.5*Cdo*density(altitude[2])*S, np.power(velocity, 2))
 
-plt.plot(velocity, d_p)
+for i in [0,1,2]:
+    plt.plot(velocity, d_p[:, i], linestyle=linVec[i], color='k')
+plt.title("Parasite Drag on a SBJ")
+plt.ylabel("Drag [lbs]")
+plt.xlabel("Velocity [ft/s]")
+plt.legend(["10,000 ft", "25,000 ft", "35,000 ft"])
+plt.grid(True)
+plt.xlim(100, 900)
+
 
 plt.figure()
 d_i = np.empty([np.size(velocity), 3])
@@ -45,6 +62,29 @@ d_i[:, 0] = np.divide(2*K*L**2, np.multiply(density(altitude[0])*S, np.power(vel
 d_i[:, 1] = np.divide(2*K*L**2, np.multiply(density(altitude[1])*S, np.power(velocity, 2)))
 d_i[:, 2] = np.divide(2*K*L**2, np.multiply(density(altitude[2])*S, np.power(velocity, 2)))
 
-plt.plot(velocity, d_i)
+for i in [0,1,2]:
+    plt.plot(velocity, d_i[:, i], linestyle=linVec[i], color='k')
+plt.title("Induced Drag on a SBJ")
+plt.ylabel("Drag [lbs]")
+plt.xlabel("Velocity [ft/s]")
+plt.legend(["10,000 ft", "25,000 ft", "35,000 ft"])
+plt.grid(True)
+plt.xlim(100, 900)
+
+plt.figure()
+d_c = np.empty([np.size(velocity), 3])
+d_c[:, 0] = np.multiply(.5*Cdo*density(altitude[2])*S, np.power(velocity, 2))+np.divide(2*K*L**2, np.multiply(S*density(altitude[2]), np.power(velocity, 2)))
+d_c[:, 1] = np.multiply(.5*Cdo*density(altitude[2])*S , np.power(velocity, 2))
+d_c[:, 2] = np.divide(2*K*L**2, np.multiply(S*density(altitude[2]), np.power(velocity, 2)))
+
+for i in [0,1,2]:
+    plt.plot(velocity, d_c[:, i], linestyle=linVec[i], color='k')
+plt.title("Drag on a SBJ at 35,000 ft")
+plt.ylabel("Drag [lbs]")
+plt.xlabel("Velocity [ft/s]")
+plt.legend(["Total", "Parasite", "Induced"])
+plt.grid(True)
+plt.xlim(100, 900)
+
 
 plt.show()
